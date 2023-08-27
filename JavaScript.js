b412f76a59ba8a31893d420e3c6cdc8e355c4f85
Anas Al-Dharei <anas202255@gmail.com>
2023-08-27 22:35:46 +0300

    HTML &  JavaScript

---
+++ JavaScript.js
@@ -0,0 +1,15 @@
+
+function myFunction(number)
+{
+    onclick = form1.answer.value += number
+}
+
+// function clear()
+// {
+//     onclick = form1.answer.value = ' '
+// }
+
+// function answer()
+// {
+//     onclick = form1.answer.value = eval(form1.answer.value)
+// }
