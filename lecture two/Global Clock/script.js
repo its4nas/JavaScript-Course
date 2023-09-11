6d99990eaa93f447cde8c75c95be82cebbed6942
Anas Al-Dharei <anas202255@gmail.com>
2023-09-11 21:33:46 +0300

    last update

---
+++ lecture two/Global Clock/script.js
@@ -0,0 +1,22 @@
+const currentTime = document.querySelector("h1")
+setInterval(() => {
+    let date = new Date(),
+    h = date.getHours(),
+    m = date.getMinutes(),
+    s = date.getSeconds(),
+    ampm = "AM";
+    if(h >= 12) {
+        h = h - 12;
+        ampm = "PM";
+    }
+    h = h == 0 ? h = 12 : h;
+    h = h < 10 ? "0" + h : h;
+    m = m < 10 ? "0" + m : m;
+    s = s < 10 ? "0" + s : s;
+    currentTime.innerText = `${h}:${m}:${s} ${ampm}`;
+
+    if (alarmTime === `${h}:${m} ${ampm}`) {
+        ringtone.play();
+        ringtone.loop = true;
+    }
+});
